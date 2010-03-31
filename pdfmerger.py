5e00ae02d763bfc6c5b2d8f6d17a01652e8c5fa3
Gautier Hayoun <ghayoun@gmail.com>
2010-03-31 12:17:52 +0100

    Added rotate option

--- pdfmerger.py
+++ pdfmerger.py
@@ -11,26 +11,34 @@ from pyPdf import PdfFileWriter, PdfFileReader
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+        opts, args = getopt.getopt(sys.argv[1:], "hr", ["help", "rotate"])
     except getopt.error, msg:
         print msg
         print "for help use --help"
         sys.exit(2)
+    docs = args
+    rotate = 0
     # process options
     for o, a in opts:
         if o in ("-h", "--help"):
             print __doc__
             sys.exit(0)
+        if o in ("-r", "--rotate"):
+            rotate = int(args[0])
+            docs = args[1:]
     if len(args) < 2:
         print __doc__
         sys.exit(0)
     # process arguments
-    outfileName = args[0]
+    outfileName = docs[0]
     output = PdfFileWriter()
-    for arg in args[1:]:
+    for arg in docs[1:]:
         input = PdfFileReader(file(arg, "rb"))
         for i in range(input.getNumPages()):
-            output.addPage(input.getPage(i))
+            page = input.getPage(i)
+            if rotate != 0:
+                page = page.rotateClockwise(rotate)
+            output.addPage(page)
     outputStream = file(outfileName, "wb")
     output.write(outputStream)
     outputStream.close()
